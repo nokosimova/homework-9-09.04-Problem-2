b431066eacf9d637e28f037fc19b59d2996cc670
unknown <nkosimova06@gmail.com>
2020-04-10 16:34:55 +0500

    create interfaces IPlayable and IRecodable

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        interface IPlayable
+        {
+            void Play();
+            void Pause();
+            void Stop();
+        } 
+        interface IRecodable
+        {
+            void Record();
+            void Pause();
+            void Stop();
+        }
+    }
+}
